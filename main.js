1f9b28009e6248746fdae35248f633de8775472a
Daisy Nardella <daisy50@live.it>
2025-02-17 14:59:56 +0100

    aggiuntoconsolelog

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let gattifila = 6;
 
 let file = Math.floor(tuttigatti / gattifila) ;
 let gattimancanti = tuttigatti % file ;
+let gattinuovafila = gattifila - gattimancanti;
 console.log(file);
 console.log(gattimancanti);
+console.log(gattinuovafila);
+
 
