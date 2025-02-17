5a12121d6bf42ee28d7c8c08b743275db0c880c3
Daisy Nardella <daisy50@live.it>
2025-02-17 14:56:59 +0100

    primo commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let tuttigatti = 44;
+let gattifila = 6;
+
+let file = Math.floor(tuttigatti / gattifila) ;
+let gattimancanti = tuttigatti % file ;
+console.log(file);
+console.log(gattimancanti);
+
